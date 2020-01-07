48ff125e2d725afd663a6a0a819dbd3135ae918f
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-07 15:58:24 -0800

    added tests for get and update

--- __tests__/reviewers.test.js
+++ __tests__/reviewers.test.js
@@ -11,9 +11,11 @@ describe('reviewer routes', () => {
   });
 
   let reviewers;
+  let reviews;
+  let films;
   beforeEach(async() => {
     mongoose.connection.dropDatabase();
-    ({ reviewers } = await testSetup());
+    ({ reviewers, reviews, films } = await testSetup());
   });
 
   afterAll(() => {
@@ -33,6 +35,62 @@ describe('reviewer routes', () => {
         expect(res.body).toEqual(
           {
             _id: expect.any(String),
+            name: 'Gene Siskel',
+            company: 'Chicago Tribune',
+          });
+      });
+  });
+
+  it('gets all reviewers', () => {
+    return request(app)
+      .get('/api/v1/reviewers/')
+      .then(res => {
+        reviewers.forEach((reviewer) => {
+          expect(res.body).toContainEqual(
+            {
+              _id: reviewer._id.toString(),
+              name: reviewer.name,
+              company: reviewer.company
+            });
+        });
+      });
+  });
+
+  it('gets a reviewer by id', () => {
+    return request(app)
+      .get(`/api/v1/reviewers/${reviewers[0]._id}`)
+      .then(res => {
+        expect(res.body).toEqual(
+          {
+            _id: reviewers[0]._id.toString(),
+            name: reviewers[0].name,
+            company: reviewers[0].company,
+            reviews: [
+              {
+                _id: reviews[0]._id.toString(),
+                rating: reviews[0].rating,
+                reviewer: reviews[0].reviewer.toString(),
+                review: reviews[0].review,
+                film: {
+                  _id: films[0]._id.toString(),
+                  title: films[0].title
+                }
+              }
+            ]
+          });
+      });
+  });
+
+  it('updates a reviewer by id', () => {
+    return request(app)
+      .patch(`/api/v1/reviewers/${reviewers[0]._id}`)
+      .send({ name: 'Richard Roeper'})
+      .then(res => {
+        expect(res.body).toEqual(
+          {
+            _id: reviewers[0]._id.toString(),
+            name: 'Richard Roeper',
+            company: reviewers[0].company,
           });
       });
   });
