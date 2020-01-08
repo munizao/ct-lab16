920c7b2d194483e2ad34255729cff894ea06ce5b
Alexandre Muniz <aom@puzzlezapper.com>
2020-01-07 17:18:56 -0800

    tests for getting reviews

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -4,6 +4,9 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const { testSetup } = require('../test-setup/setup');
+const Film = require('../lib/models/Film');
+const Review = require('../lib/models/Review');
+
 
 describe('reviewer routes', () => {
   beforeAll(() => {
@@ -44,4 +47,43 @@ describe('reviewer routes', () => {
           });
       });
   });
-})
+
+  it('gets all reviews', () => {
+    return request(app)
+      .get('/api/v1/reviews/')
+      .then(res => {
+        return Promise.all(reviews.map(async(review) => {
+          const filmDoc = await Film.findById(review.film);
+          expect(res.body).toContainEqual(
+            {
+              _id: review._id.toString(),
+              rating: review.rating,
+              review: review.review,
+              film: {
+                _id: filmDoc._id.toString(),
+                title: filmDoc.title
+              }
+            });
+        })); 
+      });
+  });
+
+  it('gets only the highest 100 reviews', async() => {
+    for(let i = 0; i < 150; i++) {
+      await Review.create({
+        rating: i % 5 + 1,
+        review: 'It was better than CATS.',
+        reviewer: reviewers[0],
+        film: films[0]
+      });
+    }
+
+    return request(app)
+      .get('/api/v1/reviews/')
+      .then(res => {
+        expect(res.body.length).toEqual(100);
+        const justRatings = res.body.map((review) => review.rating);
+        expect(justRatings).not.toContainEqual(1);
+      });
+  });
+});
